8bde9e5e11237d74217535579b7e17c41ec670c7
Vishaal Jk <vishujk9@gmail.com>
2017-06-18 11:42:47 +0530

    Update README.md

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+// Custom CSS Attachments
+
+import './styles/main.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
